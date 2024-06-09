66b64a76ce770719d56fcd37b3a63dfd0935b69c
Krish Prasad <krishprasad@Krishs-MacBook-Pro.local>
2024-06-09 13:33:22 -0700

    a

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,32 @@
 <script>
-  import Meta from "./Meta.svelte";
-  import Title from "./Components/Title.svelte";
-  import Intro from "./Components/Intro.svelte";
-  import TextAndMathEquations from "./Components/TextAndMathEquations.svelte";
-  import LineChart from "./Components/LineChart.svelte";
-  import UserInput from "./Components/UserInput.svelte";
-  // import PredictionDisplay from "./Components/PredictionDisplay.svelte";
-  import Conclusion from "./Components/Conclusion.svelte";
-  import Resources from "./Components/Resources.svelte";
-  import ScrollSide from "./Components/ScrollSide.svelte";
-  // import input from 
-</script>
+	import LineChart from './Components/LineChart.svelte';
+	import Scatterplot from './Components/scatterplot.svelte';
+	import Top10Bottom10 from './Components/Top10Bottom10.svelte';
+  </script>
+  
+  <main>
+	<h1>CEO Data Visualizations</h1>
+	<p>Lacha Barton-Gluzman, Krish Prasad, Ishaan Bal</p>
 
-
-<!-- <Meta /> -->
-<Title />
-<Intro />
-<!-- <TextAndMathEquations /> -->
-<!-- <ScrollSide /> -->
-<UserInput />
-<!-- <PredictionDisplay /> -->
-<!-- <LineChart /> -->
-<!-- <ScrollCenter /> -->
-<!-- <Conclusion /> -->
-<!-- <Resources /> -->
+	<LineChart />
+	<Scatterplot />
+	<Top10Bottom10 />
+  </main>
+  
+  <style>
+	main {
+	  width: 80%;
+	  margin: auto;
+	  text-align: center;
+	}
+	h1 {
+		font-family: Arial, sans-serif;
+		line-height: 1; /* Double spacing */
+		font-style: italic;
+		font-weight: 1500;
+		font-size: 70px;
+		/* padding-left: 400px; Adjust the padding as needed */
+		
+	}
+  </style>
+  
--- src/Components/PredictionDisplay.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    export let predictionResult;
-</script>
-
-{#if predictionResult !== undefined}
-<p>Your survival prediction: {predictionResult ? 'Survived' : 'Did Not Survive'}</p>
-{/if}
-  
